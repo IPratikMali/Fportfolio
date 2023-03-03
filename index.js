891e32d3c873e4c4935bb57af3add4ffa86c98c9
Pratik Mali <100037271+IPratikMali@users.noreply.github.com>
2023-03-03 21:05:17 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
-// Get all the links with the href attribute starting with #
-const links = document.querySelectorAll('a[href^="#"]');
-
-// Loop through each link and add the click event listener
-links.forEach(link => {
-    link.addEventListener('click', e => {
-        // Prevent the default behavior of the link
-        e.preventDefault();
-
-        // Get the target element's ID from the href attribute
-        const targetId = link.getAttribute('href');
-
-        // Get the target element
-        const targetElement = document.querySelector(targetId);
-
-        // Scroll to the target element
-        targetElement.scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+const li = document.querySelectorAll(".links");
+const div = document.querySelectorAll("div");
+function activeMenu(){
+    let len = div.length;
+    while(--len && window.scrollY + 97 < sec[len].offsetTop){
+        li.forEach(ltx => ltx.classList.remove("active"));
+        li[len].classList.add("active");
+    }
+    activeMenu();
+    window.addEventListener("scroll", activeMenu)
+}
